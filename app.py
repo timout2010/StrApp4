ef6b55b7925f30884a28b7f72dbf1115f56607cf
timout2010 <40605936+timout2010@users.noreply.github.com>
2024-12-21 00:24:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ from io import BytesIO
 import ast
 # Configuration№
 # FUNCTION_BASE_URL = "http://localhost:7190/api" # e.g., https://<function-app>.azurewebsites.net/api/
-version="0.93a"
+version="0.931a"
 FUNCTION_BASE_URL = "https://glauditazurefuntion.azurewebsites.net/api" # e.g., https://<function-app>.azurewebsites.net/api/
 
 GENERATE_SAS_TOKEN_ENDPOINT = f"{FUNCTION_BASE_URL}/GenerateSASToken"
