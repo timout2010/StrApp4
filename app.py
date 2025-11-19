518325256d31b8a855a62b413873287a7ebedc2c
timout2010 <40605936+timout2010@users.noreply.github.com>
2025-11-20 00:30:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ def main():
             "Pre LLM threshold",
             min_value=0.0,
             max_value=1.0,
-            value=0.81,
+            value=0.79,
             step=0.01,
             format="%.2f",
         )
@@ -183,7 +183,7 @@ def main():
             "TopK candidates",
             min_value=1,
             max_value=500,
-            value=250,
+            value=50,
             step=1,
         )
 
